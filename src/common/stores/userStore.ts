17459a1c380e910ae3deffffffe2221dd86ca18d
Jekabs Leinis <jekabs@leinis.com>
2023-11-08 18:01:19 +0200

    Setup localStorage for user

--- src/common/stores/userStore.ts
+++ src/common/stores/userStore.ts
@@ -3,7 +3,9 @@ import { defineStore } from 'pinia'
 import { User } from '@/common/models/userModels'
 
 export const useUserStore = defineStore('user', () => {
-  const currentUser = ref(new User())
+  const localUser = localStorage.getItem('currentUser')
+
+  const currentUser = ref(new User(localUser ? JSON.parse(localUser) : {}))
   const isLoggedIn = computed(() => Boolean(currentUser.value.id))
 
   const loginModalVisible = ref(false)
@@ -11,6 +13,8 @@ export const useUserStore = defineStore('user', () => {
 
   const setUser = (user: User) => {
     currentUser.value = user
+
+    localStorage.setItem('currentUser', JSON.stringify(currentUser.value))
   }
 
   return { currentUser, isLoggedIn, setUser, loginModalVisible, registrationModalVisible }
