7f20e2ba28a24a89b8d5d21194c8679f4be423f0
mouniss <monika.vainina@gmail.com>
2023-11-09 00:37:33 +0200

    Bez avatāra sejas

--- src/chat/views/ChatModal.vue
+++ src/chat/views/ChatModal.vue
@@ -19,8 +19,6 @@
                         <p class="small mb-1 text-muted">{{ message.time }}</p>
                     </div>
                     <div class="d-flex flex-row justify-content-start">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava5-bg.webp"
-                            alt="avatar 1" class="avatar-img" />
                         <div class="message-content">
                             {{ message.content }}
                         </div>
@@ -99,10 +97,6 @@ const sendMessage = () => {
     margin-bottom: 10px;
 }
 
-.avatar-img {
-    width: 45px;
-    height: 100%;
-}
 
 .message-content {
     padding: 10px;
