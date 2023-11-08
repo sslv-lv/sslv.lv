fe69ca08093741c9a619113e62b0c7a0ce9b86b3
Reinis <Reinis.Velmeris@edu.rtu.lv>
2023-11-09 00:25:56 +0200

    added filtering, and create button

--- src/advertisements/views/CommonList.vue
+++ src/advertisements/views/CommonList.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, onMounted, computed, onUnmounted } from 'vue'
+import { ref, onMounted, computed, onUnmounted, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
 import { collection, query, where, getDocs, orderBy, startAfter, limit } from 'firebase/firestore'
 import type { QueryDocumentSnapshot, DocumentData } from 'firebase/firestore'
@@ -41,26 +41,31 @@ const selectedSort = ref('newest') // Default sort
 const lastVisible = ref<QueryDocumentSnapshot<DocumentData> | null>(null)
 const allAdsLoaded = ref(false)
 const listingsRef = collection(db, 'listings')
+const filters = ref<Record<string, string>>({})
 
 const fetchFieldsForCurrentSubcategory = async (categoryName: string, subcategoryName: string) => {
-  const categoriesRef = collection(db, 'categories')
-  const q = query(categoriesRef, where('name', '==', categoryName))
+  const categoriesRef = collection(db, 'categories');
+  const q = query(categoriesRef, where('name', '==', categoryName));
 
   try {
-    const querySnapshot = await getDocs(q)
+    const querySnapshot = await getDocs(q);
     const categoryData = querySnapshot.docs
       .map((doc) => doc.data() as Category)
-      .find((cat) => cat.name === categoryName)
-    const subcategoryData = categoryData?.subcategories.find((sub) => sub.name === subcategoryName)
-    fields.value = subcategoryData ? subcategoryData.fields : []
-  } catch (error) {
-    console.error('Error fetching subcategory fields: ', error)
-  }
-}
+      .find((cat) => cat.name === categoryName);
 
-const applyFilter = (selectedField: Field) => {
-  // TODO
-}
+    const subcategoryData = categoryData?.subcategories.find((sub) => sub.name === subcategoryName);
+    if (subcategoryData && subcategoryData.fields) {
+      fields.value = subcategoryData.fields;
+
+      subcategoryData.fields.forEach(field => {
+        filters.value[field.name] = '';
+      });
+    }
+  } catch (error) {
+    console.error('Error fetching subcategory fields: ', error);
+  }
+};
+
 
 const fetchAdvertisements = async (subcategoryName: string) => {
   let q
@@ -110,18 +115,30 @@ const loadMoreAdvertisements = () => {
   }
 }
 
-const sortedAdvertisements = computed(() => {
+const sortedAndFilteredAdvertisements = computed(() => {
+  // First, filter the advertisements
+  const filtered = advertisements.value.filter((ad) => {
+    return Object.entries(filters.value).every(([key, value]) => {
+      // If the filter is empty, do not filter by this property.
+      if (value === '') return true;
+
+      // Check if the advertisement has the property and if it includes the filter value.
+      return ad[key] !== undefined && ad[key].toString().toLowerCase().includes(value.toLowerCase());
+    });
+  });
+
+  // Then, sort the filtered advertisements
   switch (selectedSort.value) {
     case 'mostVisitors':
-      return [...advertisements.value].sort((a, b) => b.visitors - a.visitors)
+      return filtered.sort((a, b) => b.visitors - a.visitors);
     case 'newest':
-      return [...advertisements.value].sort((a, b) => b.created - a.created)
+      return filtered.sort((a, b) => b.created - a.created);
     case 'cheapest':
-      return [...advertisements.value].sort((a, b) => a.price - b.price)
+      return filtered.sort((a, b) => a.price - b.price);
     default:
-      return advertisements.value
+      return filtered;
   }
-})
+});
 
 const isSidebarVisible = ref(true)
 
@@ -142,79 +159,64 @@ onMounted(async () => {
 onUnmounted(() => {
   window.removeEventListener('scroll', loadMoreAdvertisements)
 })
+
+
+///=====================================FILTERING=======================================
+
 </script>
 
 <template>
   <div class="container-fluid d-flex flex-row h-100">
     <div class="row">
       <!-- Collapsible sidebar content -->
-      <div
-        class="sidebar sticky-top"
-        :class="[isSidebarVisible ? 'show' : 'collapse']"
-        id="filterSidebar"
-      >
+      <div class="sidebar sticky-top" :class="[isSidebarVisible ? 'show' : 'collapse']" id="filterSidebar">
         <div class="position-sticky pt-3 ps-3">
           <h5>Filtri</h5>
           <ul class="nav flex-column">
             <li class="nav-item" v-for="(field, index) in fields" :key="index">
               <label class="nav-link">
                 {{ field.name }}
-                <input
-                  type="text"
-                  class="form-control"
-                  v-model="field.value"
-                  @input="applyFilter(field)"
-                />
+                <input type="text" class="form-control" v-model="filters[field.name]" />
               </label>
             </li>
           </ul>
         </div>
       </div>
+
     </div>
     <main class="flex-grow-1 listing-container">
       <h1 class="ps-4">Sludinājumi</h1>
+      <div class="d-flex justify-content-end">
+        <RouterLink :to="{ name: 'AdCreation' }">
+          <button type="button" class="btn custom-btn">Ievietot sludinājumu</button>
+        </RouterLink>
+      </div>
+
+
+
+
       <div class="container mt-4">
         <ul class="nav nav-tabs">
           <li class="nav-item">
-            <a
-              class="nav-link"
-              :class="{ active: selectedSort === 'newest' }"
-              @click.prevent="selectedSort = 'newest'"
-              >Jaunākie</a
-            >
+            <a class="nav-link" :class="{ active: selectedSort === 'newest' }"
+              @click.prevent="selectedSort = 'newest'">Jaunākie</a>
           </li>
           <li class="nav-item">
-            <a
-              class="nav-link"
-              :class="{ active: selectedSort === 'mostVisitors' }"
-              @click.prevent="selectedSort = 'mostVisitors'"
-              >Skatītākie</a
-            >
+            <a class="nav-link" :class="{ active: selectedSort === 'mostVisitors' }"
+              @click.prevent="selectedSort = 'mostVisitors'">Skatītākie</a>
           </li>
           <li class="nav-item">
-            <a
-              class="nav-link"
-              :class="{ active: selectedSort === 'cheapest' }"
-              @click.prevent="selectedSort = 'cheapest'"
-              >Lētākie</a
-            >
+            <a class="nav-link" :class="{ active: selectedSort === 'cheapest' }"
+              @click.prevent="selectedSort = 'cheapest'">Lētākie</a>
           </li>
         </ul>
-        <div v-if="sortedAdvertisements.length > 0">
+        <div v-if="sortedAndFilteredAdvertisements.length > 0">
           <div class="list-group rounded-bottom">
-            <router-link
-              v-for="ad in sortedAdvertisements"
-              :key="ad.id"
+            <router-link v-for="ad in sortedAndFilteredAdvertisements" :key="ad.id"
               :to="`/${ad.category}/${ad.subcategory}/${ad.id}`"
-              class="list-group-item list-group-item-action d-flex align-items-center"
-            >
-              <img
-                v-if="ad.images && ad.images.length > 0"
-                :src="ad.images[0]"
-                class="img-fluid rounded-start"
-                alt="Advertisement image"
-                style="width: 100px; height: auto"
-              />
+              class="list-group-item list-group-item-action d-flex align-items-center">
+              <img v-if="ad.images && ad.images.length > 0" :src="ad.images[0]" class="img-fluid rounded-start"
+                alt="Advertisement image" style="width: 100px; height: auto" />
               <div class="ms-3 me-auto">
                 <div class="fw-bold">{{ ad.title }}</div>
                 <div class="description-wrapper">
@@ -314,6 +316,20 @@ main {
   background-color: #748cab;
   border: none;
   transition: left 0.3s;
+}
+
+.custom-btn {
+  background-color: #748cab;
+  color: white;
+}
+
+.custom-btn:hover {
+  background-color: #3e5c76;
+  color: #fff;
+}
+
+.nav-link {
+  color: #748cab;
 }
 
 .my-custom-bg {
